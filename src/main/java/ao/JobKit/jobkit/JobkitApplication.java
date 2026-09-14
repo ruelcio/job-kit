82974fac4c6b99a5c14fa80c9f49b65551454f66
ruelcio <ruelciojm@gmail.com>
2026-09-14 17:55:50 +0100

    first commit: conecting neon db

---
+++ src/main/java/ao/JobKit/jobkit/JobkitApplication.java
@@ -0,0 +1,13 @@
+package ao.JobKit.jobkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/JobKit/jobkit/JobkitApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.JobKit.jobkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
